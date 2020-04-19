c6193ce0c0b97f9549b8b72853a8b76ef275175f
MatiasStorm <mat-435@hotmail.com>
2020-04-19 18:21:57 +0200

    Added pc app

---
+++ PCApp/src/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import javax.bluetooth.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Working");
+
+    }
+}
